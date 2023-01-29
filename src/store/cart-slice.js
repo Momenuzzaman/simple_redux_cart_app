c4b1e113caaa663d7676cde6a93edcc53fdffb1d
mdemon420 <mdeamon4@gmail.com>
2023-01-29 15:19:25 +0600

    export actions

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -24,6 +24,20 @@ const cartSlice = createSlice({
                 existingItem.totalPrice += newItem.price;
             }
         },
-        removeItemCart() { }
+        removeItemCart(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find(item => item.id === id);
+            state.quantity--;
+            state.changed = true;
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter(item => item.id !== id);
+            }
+            else {
+                existingItem.quantity--;
+                existingItem.totalPrice -= existingItem.price;
+            }
+        }
     }
 });
+export const cartActions = cartSlice.actions
+export default cartSlice.reducer;
